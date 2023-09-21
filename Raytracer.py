2dc0f95f4be337a9e66bd84ea371f3ab11be712d
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-09-21 01:32:45 -0600

    RT1 finished

--- Raytracer.py
+++ Raytracer.py
@@ -16,17 +16,48 @@ screen = pygame.display.set_mode((width,height),pygame.DOUBLEBUF | pygame.HWACCE
 screen.set_alpha(None)
 
 raytracer = Raytracer(screen)
-raytracer.rtClearColor(0.25,0.25,0.25)
+raytracer.rtClearColor(0.741,0.878,0.996)
 
-brick = Material(diffuse=(1,0.4,0.4),spec=8)
-grass = Material(diffuse=(0.4,1,0.4),spec=32)
-water = Material(diffuse=(0.4,0.4,1),spec=256)
+#TESTS
+# brick = Material(diffuse=(1,0.4,0.4),spec=8, Ks= 0.01)
+# grass = Material(diffuse=(0.4,1,0.4),spec=32, Ks= 0.1)
+# water = Material(diffuse=(0.4,0.4,1),spec=256, Ks= 0.2)
 
-raytracer.scene.append(Sphere(position=(-2,0,-5), radius=0.5, material = brick))
-raytracer.scene.append(Sphere(position=(0,0,-5), radius=0.5, material = grass))
-raytracer.scene.append(Sphere(position=(2,0,-5), radius=0.5, material = water))
-raytracer.lights.append(AmbientLight(intensity=0.1))
-raytracer.lights.append(DirectionalLight(direction=(0,-1,-1), intensity=0.7))
+# raytracer.scene.append(Sphere(position=(1,1,-5), radius=0.5, material = grass))
+# raytracer.scene.append(Sphere(position=(0,0,-7), radius=2, material = brick))
+# raytracer.scene.append(Sphere(position=(0.5,-1,-5), radius=0.3, material = water))
+# raytracer.lights.append(AmbientLight(intensity=0.1))
+# raytracer.lights.append(DirectionalLight(direction=(-1,-1,-1), intensity=0.7))
+# raytracer.lights.append(PointLight(point=(2.5,0,-5),intensity=0.5,color=(1,0,1)))
+
+#RT1
+snow = Material(diffuse=(1,1,1), spec=8, Ks=0.07)
+carbon = Material(diffuse=(0,0,0), spec=8, Ks=0.07)
+carrot = Material(diffuse=(0.984,0.521,0), spec=8, Ks=0.07)
+
+#spheres
+#body
+raytracer.scene.append(Sphere(position=(0,0.15,-9), radius=1.3, material=snow))
+raytracer.scene.append(Sphere(position=(0,-2.2,-9), radius=1.7, material=snow))
+#face
+raytracer.scene.append(Sphere(position=(0,1.65,-9), radius=1, material=snow))
+raytracer.scene.append(Sphere(position=(0.4,1.6,-7.8), radius=0.15, material=carbon))
+raytracer.scene.append(Sphere(position=(-0.4,1.6,-7.8), radius=0.15, material=carbon))
+raytracer.scene.append(Sphere(position=(0,1.5,-7.8), radius=0.15, material=carrot))
+raytracer.scene.append(Sphere(position=(-0.4,1.25,-7.8), radius=0.07, material=carbon))
+raytracer.scene.append(Sphere(position=(0.4,1.25,-7.8), radius=0.07, material=carbon))
+raytracer.scene.append(Sphere(position=(-0.25,1.15,-7.8), radius=0.07, material=carbon))
+raytracer.scene.append(Sphere(position=(0.25,1.15,-7.8), radius=0.07, material=carbon))
+raytracer.scene.append(Sphere(position=(0,1.1,-7.8), radius=0.07, material=carbon))
+#buttons
+raytracer.scene.append(Sphere(position=(0,0.25,-6.9), radius=0.15, material=carbon))
+raytracer.scene.append(Sphere(position=(0,-0.85,-6.9), radius=0.17, material=carbon))
+raytracer.scene.append(Sphere(position=(0,-2.2,-6.9), radius=0.19, material=carbon))
+
+#lights
+raytracer.lights.append(AmbientLight(intensity=0.9))
+raytracer.lights.append(PointLight(point=(2.5,0,-5),intensity=0.7,color=(0.937,0.137,0.235)))
+raytracer.lights.append(PointLight(point=(-2.5,2,-5),intensity=0.8,color=(1,0.862,0.521)))
 
 isRunning = True
 while isRunning:
