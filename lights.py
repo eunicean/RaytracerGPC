263ec59bebce12d9b3e4fff0263e83aebca6e4fe
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-09-20 22:03:40 -0600

    added new functions to lights class and diffuse class

--- lights.py
+++ lights.py
@@ -1,8 +1,17 @@
+import meth
 class Light(object):
     def __init__(self, intensity = 1, color = (1,1,1), lightType = "None"):
         self.intensity = intensity
         self.color = color
         self.lightType = lightType
+
+    def getLightColor(self):
+        return [self.color[0]*self.intensity,
+                self.color[0]*self.intensity,
+                self.color[0]*self.intensity]
+    
+    def getDiffuseColor(self, intercept):
+        return self.getLightColor()
 
 class AmbientLight(Light):
     def __init__(self, intensity=1, color=(1, 1, 1)):
@@ -10,5 +19,14 @@ class AmbientLight(Light):
 
 class DirectionalLight(Light):
     def __init__(self, direction = (0,-1,0), intensity=1, color=(1, 1, 1)):
-        self.direction = direction
+        self.direction = meth.normalizeVector(direction)
         super().__init__(intensity, color, "Directional")
+
+    def getDiffuseColor(self, intercept):
+        lightColor = super().getDiffuseColor(intercept)
+        dir = [i*-1 for i in self.direction]
+        intensity = meth.dotProd(intercept.normal, dir) * self.intensity
+        intensity = max(0,min(1,intensity))
+
+        diffuseColor = [(i*intensity) for i in self.color]
+        return diffuseColor
