127fea3de9220805065ca2d90865b83718656437
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-09-20 23:55:42 -0600

    Ks value added

--- materials.py
+++ materials.py
@@ -1,4 +1,5 @@
 class Material(object):
-    def __init__(self, diffuse = (1,1,1), spec = 1.0):
+    def __init__(self, diffuse = (1,1,1), spec = 1.0, Ks = 0.01):
         self.diffuse = diffuse
         self.spec = spec #determines how perfect will be a surface
+        self.Ks = Ks #specular coeficent
