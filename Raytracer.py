d2bf498dad4f5657ef10cf03cfb7e09dcbf41a1e
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-10-12 20:37:07 -0600

    chanes

--- Raytracer.py
+++ Raytracer.py
@@ -7,8 +7,8 @@ from figures import *
 from lights import *
 from materials import *
 
-width = 256
-height = 256
+width = 128
+height = 128
 
 pygame.init()
 
@@ -29,7 +29,7 @@ marbleTexture = pygame.image.load("textures/marble.jpg")
 brick = Material(diffuse=(1,0.4,0.4),spec=8, Ks= 0.01)
 grass = Material(diffuse=(0.4,1,0.4),spec=32, Ks= 0.1)
 water = Material(diffuse=(0.4,0.4,1),spec=256, Ks= 0.2, ior=4,matType=TRANSPARENT)
-mirror = Material(diffuse=(0.9,0.9,0.9),spec=64, Ks=0.2,matType=TRANSPARENT)
+mirror = Material(diffuse=(0.9,0.9,0.9),spec=64, Ks=0.2,matType=REFLECTIVE)
 blueMirror = Material(diffuse=(0.4,0.4,0.9),spec=32, Ks=0.15,matType=REFLECTIVE)
 jupiter = Material(texture=jupiterTexture)
 marble = Material(texture=marbleTexture, spec=64, Ks=0.1, matType=REFLECTIVE)
@@ -37,18 +37,23 @@ glass = Material(diffuse=(0.9,0.9,0.9),spec=64, Ks=0.1, ior=1.5, matType=TRANSPA
 diamond = Material(diffuse=(0.9,0.9,0.9),spec=128, Ks=0.2, ior=2.417, matType=TRANSPARENT)
 jupiter = Material(texture=jupiterTexture)
 jupiterglassball = Material(texture=jupiterTexture, spec=64, Ks=0.1,matType= REFLECTIVE)
+canica = Material(texture=marbleTexture, diffuse=(0.8,0.8,1.0), spec=64, Ks=0.15, ior=1.5, matType=TRANSPARENT)
 
 # raytracer.scene.append(Sphere(position=(0.5,-1,-5), radius=0.3, material = water))
 
 # raytracer.scene.append(Sphere(position=(-2,0,-7), radius=1.5, material = marble))
 # raytracer.scene.append(Sphere(position=(0,-1,-5), radius=0.5, material = mirror))
 
-raytracer.scene.append(Sphere(position= (0,1,-5), radius= 0.85, material= water))
-raytracer.scene.append(Sphere(position= (0,-1,-5), radius= 0.85, material= diamond))
-raytracer.scene.append(Sphere(position= (-2,1,-5), radius= 0.85, material= brick))
-raytracer.scene.append(Sphere(position=(-2,-1,-5), radius=0.85, material = jupiter))
-raytracer.scene.append(Sphere(position=(2,1,-5), radius=0.85, material = mirror))
-raytracer.scene.append(Sphere(position=(-2,-1,-5), radius=0.85, material = jupiterglassball))
+# raytracer.scene.append(Sphere(position= (0,1,-5), radius= 0.85, material= water))
+# raytracer.scene.append(Sphere(position= (0,-1,-5), radius= 0.85, material= diamond))
+# raytracer.scene.append(Sphere(position= (-2,1,-5), radius= 0.85, material= brick))
+# raytracer.scene.append(Sphere(position=(-2,-1,-5), radius=0.85, material = jupiter))
+# raytracer.scene.append(Sphere(position=(2,1,-5), radius=0.85, material = mirror))
+# raytracer.scene.append(Sphere(position=(-2,-1,-5), radius=0.85, material = jupiterglassball))
+
+raytracer.scene.append(Sphere(position=(0,0,-5), radius=1.0,material=canica))
+raytracer.scene.append(Plane(position=(0,-5,0),normal=(0,1,0),material=brick))
+raytracer.scene.append(Disk(position=(0,-1.5,-5),normal=(0,1,0),radius=1.5,material=mirror))
 
 raytracer.lights.append(AmbientLight(intensity=0.1))
 raytracer.lights.append(DirectionalLight(direction=(-1,-1,-1), intensity=0.9))
@@ -69,6 +74,3 @@ while isRunning:
                 isRunning = False
 
 pygame.quit()
-
-#falla: solo dibuja el circulo en negro en vez de refractar
-#toca: clase 28-09
--- figures.py
+++ figures.py
@@ -54,3 +54,53 @@ class Sphere(Shape):
                          normal= normal,
                          texcoords= (u,v),
                          obj= self)
+    
+class Plane(Shape):
+    def __init__(self, position, material, normal):
+        self.normal = meth.normalizeVector(normal)
+        super().__init__(position, material)
+
+    def ray_intersect(self, orig, dir):
+        #Distancia = (planepPos - origRay) o normal) / (dirRay o normal1``)
+        denom = meth.dotProd(dir,self.normal)
+
+        if abs(denom) <= 0.0001:
+            return None
+        
+        num = meth.dotProd(meth.substractionVectors(self.position,orig),self.normal)
+        t = num/denom
+
+        if t < 0:
+            return None
+        
+        #P = O + D * t
+        P = meth.additionVectors(orig,meth.multiplyValueAndVector(t,dir))
+
+        return Intercept(distance= t,
+                         point= P,
+                         normal= self.normal,
+                         texcoords= None,
+                         obj= self)
+    
+class Disk(Plane):
+    def __init__(self, position, material, normal, radius):
+        self.radius = radius
+        super().__init__(position, material, normal)
+
+    def ray_intersect(self, orig, dir):
+        planeIntersect = super().ray_intersect(orig,dir) #if does contact to the plane which it belongs
+
+        if planeIntersect is None:
+            return None
+        
+        contactDistance =  meth.substractionVectors(planeIntersect.point,self.position) #vector
+        contactDistance = meth.magnitudOfVector(contactDistance)
+
+        if contactDistance > self.radius:
+            return None
+        
+        return Intercept(distance= planeIntersect.distance,
+                         point= planeIntersect.point,
+                         normal= self.normal,
+                         texcoords= None,
+                         obj= self)
