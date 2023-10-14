40b1fd8f8b5efb386da4dc352c40f0ff2ef4c86e
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-10-13 19:25:54 -0600

    triagles added

--- figures.py
+++ figures.py
@@ -174,3 +174,47 @@ class AABB(Shape):
                          normal= intersect.normal,
                          texcoords= (u,v),
                          obj= self)
+    
+# Help of chatGPT:
+# https://chat.openai.com/share/806e6b97-c8c8-49a2-874f-cc1aa6c2b7a1
+class Triangle(Shape):
+    def __init__(self,material, verts): #In this, verts[0] = (x,y,z) will determine the position
+        self.verts = verts #every value in verts is an (x,y,z)
+        super().__init__(verts[0], material)
+
+    def ray_intersect(self, orig, dir):
+        edge1 = meth.substractionVectors(self.verts[1],self.verts[0])
+        edge2 = meth.substractionVectors(self.verts[2],self.verts[0])
+        normal = meth.normalizeVector(meth.prodCrossV(edge1,edge2))
+
+        denom = meth.dotProd(normal,dir) #this is h and a combined
+        if abs(denom) <= 0.001:
+            return None
+
+        # d = meth.dotProd(normal,self.verts[0])
+        # n = meth.dotProd(normal,orig)
+        
+        scalingFactor = 1.0 / meth.dotProd(edge1, meth.prodCrossV(dir,edge2))
+        d = meth.substractionVectors(orig, self.verts[0])
+        u = scalingFactor * meth.dotProd(d, meth.prodCrossV(dir,edge2)) #barycentric coordinate 
+        orientation = meth.prodCrossV(d, edge1)
+        v = scalingFactor * meth.dotProd(dir, orientation)# barycentric coordinate 
+
+        if u < 0.0 or u > 1.0:
+            return None
+        if v < 0.0 or u + v > 1.0:
+            return None
+
+        # At this point, we have a valid intersection
+        t = scalingFactor * meth.dotProd(edge2, orientation)
+        if t <= 0:
+            return None
+
+        # Compute the intersection point and return the result
+        P = meth.additionVectors(orig, meth.multiplyValueAndVector(t, dir))
+
+        return Intercept(distance=t,
+                         point=P,
+                         normal=normal,
+                         texcoords=None,
+                         obj=self)
